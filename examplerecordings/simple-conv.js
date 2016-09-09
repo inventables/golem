0e072e168b58c55ab61c1034faa50c67a3333135
David Altenburg <david@inventables.com>
2016-09-09 17:04:06 -0500

    Save recordings in the new script format

---
+++ examplerecordings/simple-conv.js
@@ -0,0 +1,34 @@
+g = org.inventables.golem;
+
+escape = require('js-string-escape');
+util = require('util');
+
+g.output("0");
+g.expect(g.literal("2[\"message\",\"Bob\"]"), function(out, msg) {
+  var colorMessage = {"type":"color", "data": "magenta"};
+  var helloMessage = {"type": "message", "data": {"time": 1472834316527,
+                                                  "author": "system",
+                                              "text": "Hello, Bob"}};
+
+  out(util.format('2["message","%s"]', escape(JSON.stringify(colorMessage))));
+  out(util.format('2["message","%s"]', escape(JSON.stringify(helloMessage))));
+});
+
+g.expect(g.literal('2["message","Hi there"]'), function(out, msg) {
+  var res = {"type": "message",
+             "data": {"time": 1472834318896,
+                      "text": "Hi there",
+                      "author": "Bob",
+                      "color": "magenta"}};
+
+  out(util.format('2["message", "%s"]', escape(JSON.stringify(res))));
+});
+
+g.expect(g.literal('2["message","/time"]'), function(out, msg) {
+  var res = {"type": "message",
+             "data": {"time":1472834320618,
+                      "text":"2016-09-02T16:38:40.618Z",
+                      "author": "system",
+                      "color": "black"}};
+  out(util.format('2["message","%s"]', escape(JSON.stringify(res))));
+});
--- savedrecording.js
+++ savedrecording.js
@@ -1,34 +1,16 @@
 g = org.inventables.golem;
 
-escape = require('js-string-escape');
-util = require('util');
-
 g.output("0");
 g.expect(g.literal("2[\"message\",\"Bob\"]"), function(out, msg) {
-  var colorMessage = {"type":"color", "data": "magenta"};
-  var helloMessage = {"type": "message", "data": {"time": 1472834316527,
-                                                  "author": "system",
-                                              "text": "Hello, Bob"}};
+  out("2[\"message\",\"{\\\"type\\\":\\\"color\\\",\\\"data\\\":\\\"green\\\"}\"]");
+  out("2[\"message\",\"{\\\"type\\\":\\\"message\\\",\\\"data\\\":{\\\"time\\\":1473458540874,\\\"text\\\":\\\"Hello, Bob\\\",\\\"author\\\":\\\"system\\\",\\\"color\\\":\\\"black\\\"}}\"]");
+ });
 
-  out(util.format('2["message","%s"]', escape(JSON.stringify(colorMessage))));
-  out(util.format('2["message","%s"]', escape(JSON.stringify(helloMessage))));
-});
+g.expect(g.literal("2[\"message\",\"Hi there\"]"), function(out, msg) {
+  out("2[\"message\",\"{\\\"type\\\":\\\"message\\\",\\\"data\\\":{\\\"time\\\":1473458542354,\\\"text\\\":\\\"Hi there\\\",\\\"author\\\":\\\"Bob\\\",\\\"color\\\":\\\"green\\\"}}\"]");
+ });
 
-g.expect(g.literal('2["message","Hi there"]'), function(out, msg) {
-  var res = {"type": "message",
-             "data": {"time": 1472834318896,
-                      "text": "Hi there",
-                      "author": "Bob",
-                      "color": "magenta"}};
+g.expect(g.literal("2[\"message\",\"/time\"]"), function(out, msg) {
+  out("2[\"message\",\"{\\\"type\\\":\\\"message\\\",\\\"data\\\":{\\\"time\\\":1473458543813,\\\"text\\\":\\\"2016-09-09T22:02:23.813Z\\\",\\\"author\\\":\\\"system\\\",\\\"color\\\":\\\"black\\\"}}\"]");
+ });
 
-  out(util.format('2["message", "%s"]', escape(JSON.stringify(res))));
-});
-
-g.expect(g.literal('2["message","/time"]'), function(out, msg) {
-  var res = {"type": "message",
-             "data": {"time":1472834320618,
-                      "text":"2016-09-02T16:38:40.618Z",
-                      "author": "system",
-                      "color": "black"}};
-  out(util.format('2["message","%s"]', escape(JSON.stringify(res))));
-});
