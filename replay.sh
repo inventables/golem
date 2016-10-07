084b2d1334ac20b9c161ab93ac33573dc76a3e8e
David Altenburg <david@inventables.com>
2016-10-07 15:09:52 -0500

    refactoring to support rules

---
+++ replay.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+node golem.js --sourcePort 2338 --targetPort 2337 --isReplay true
--- savedrecording.js
+++ savedrecording.js
@@ -8,7 +8,15 @@ g.expect(g.literal("2[\"message\",\"Bill\"]"), function(out, msg) {
 
 g.expect(g.literal("2[\"message\",\"Hi there\"]"), function(out, msg) {
   out("2[\"message\",\"{\\\"type\\\":\\\"message\\\",\\\"data\\\":{\\\"time\\\":1475867789608,\\\"text\\\":\\\"Hi there\\\",\\\"author\\\":\\\"Bill\\\",\\\"color\\\":\\\"red\\\"}}\"]");
- });
+});
+
+g.when(g.literal("2[\"message\",\"Knock knock\"]"), function(out, msg) {
+
+  out("2[\"message\",\"{\\\"type\\\":\\\"message\\\",\\\"data\\\":{\\\"time\\\":1475867789608,\\\"text\\\":\\\"Knock knock\\\",\\\"author\\\":\\\"Bill\\\",\\\"color\\\":\\\"red\\\"}}\"]");
+
+  out("2[\"message\",\"{\\\"type\\\":\\\"message\\\",\\\"data\\\":{\\\"time\\\":1475867791634,\\\"text\\\":\\\"Who's there?\\\",\\\"author\\\":\\\"system\\\",\\\"color\\\":\\\"black\\\"}}\"]");
+});
+
 
 g.expect(g.literal("2[\"message\",\"/time\"]"), function(out, msg) {
   out("2[\"message\",\"{\\\"type\\\":\\\"message\\\",\\\"data\\\":{\\\"time\\\":1475867791634,\\\"text\\\":\\\"2016-10-07T19:16:31.634Z\\\",\\\"author\\\":\\\"system\\\",\\\"color\\\":\\\"black\\\"}}\"]");
@@ -17,4 +25,3 @@ g.expect(g.literal("2[\"message\",\"/time\"]"), function(out, msg) {
 g.expect(g.literal("2[\"message\",\"Bye\"]"), function(out, msg) {
   out("2[\"message\",\"{\\\"type\\\":\\\"message\\\",\\\"data\\\":{\\\"time\\\":1475867794263,\\\"text\\\":\\\"Bye\\\",\\\"author\\\":\\\"Bill\\\",\\\"color\\\":\\\"red\\\"}}\"]");
  });
-
