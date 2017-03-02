7b138892ce083f31b287514bb24c3cd93d2d3ef9
David Altenburg <david@inventables.com>
2017-03-01 21:02:44 -0600

    Streamlined chat example script

--- examples/chatbot/conv-with-rules.js
+++ examples/chatbot/conv-with-rules.js
@@ -3,49 +3,61 @@ g = com.inventables.golem;
 escape = require('js-string-escape');
 util = require('util');
 
+
+var sio = {
+  inPattern: function(regex) {
+    param = regex.toString().slice(1, -1);
+    return new RegExp('^2\\[\\"message\\",\\"' + param + '\\"\\]$');
+  },
+  inMessage: function(msg) {
+    return '2["message","' + msg + '"]';
+  },
+  outMessage: function(msg) {
+    return util.format('2["message","%s"]', escape(JSON.stringify(msg)));
+  }
+};
+
 g.output("0");
-g.expect(g.pattern(/^2\[\"message\",\"(\w+)\"\]$/), function(out, msg, match) {
+g.expect(g.pattern(sio.inPattern(/(\w+)/)), function(out, msg, match) {
   var colorMessage = {"type":"color", "data": "magenta"};
   var helloMessage = {"type": "message", "data": {"time": 1472834316527,
                                                   "author": "system",
                                               "text": "Hello, " + match}};
 
-  out(util.format('2["message","%s"]', escape(JSON.stringify(colorMessage))));
-  out(util.format('2["message","%s"]', escape(JSON.stringify(helloMessage))));
+  out(sio.outMessage(colorMessage));
+  out(sio.outMessage(helloMessage));
 });
 
-g.when(g.literal('2["message","What\'s up?"]'), function(out, msg) {
+g.when(g.literal(sio.inMessage("What\'s up?")), function(out, msg) {
   var replyMessage = {"type": "message", "data": {"time": 1472834316527,
                                                   "author": "system",
                                                   "text": "Not too much"}};
-
-  out(util.format('2["message","%s"]', escape(JSON.stringify(replyMessage))));
+  out(sio.outMessage(replyMessage));
 });
 
-g.when(g.pattern(/^2\["message","I'm ([\w\s]+)"\]$/), function(out, msg, match) {
-  var replyMessage = {"type": "message", "data": {"time": 1472834316527,
-                                                  "author": "system",
-                                                  "text": "No, I&apos;M " + match}};
-
-  out(util.format('2["message","%s"]', escape(JSON.stringify(replyMessage))));
-
-});
-
-g.expect(g.literal('2["message","Hi there"]'), function(out, msg) {
+g.expect(g.literal(sio.inMessage("Hi there")), function(out, msg) {
   var res = {"type": "message",
              "data": {"time": 1472834318896,
                       "text": "Hi there",
                       "author": "Bob",
                       "color": "magenta"}};
 
-  out(util.format('2["message", "%s"]', escape(JSON.stringify(res))));
+  out(sio.outMessage(res));
 });
 
-g.expect(g.literal('2["message","/time"]'), function(out, msg) {
+g.when(g.pattern(sio.inPattern(/I'm ([\w\s]+)/)), function(out, msg, match) {
+  var replyMessage = {"type": "message", "data": {"time": 1472834316527,
+                                                  "author": "system",
+                                                  "text": "No, I&apos;M " + match}};
+
+  out(sio.outMessage(replyMessage));
+});
+
+g.expect(g.literal(sio.inMessage("/time")), function(out, msg) {
   var res = {"type": "message",
              "data": {"time":1472834320618,
                       "text":"2016-09-02T16:38:40.618Z",
                       "author": "system",
                       "color": "black"}};
-  out(util.format('2["message","%s"]', escape(JSON.stringify(res))));
+  out(sio.outMessage(res));
 });
